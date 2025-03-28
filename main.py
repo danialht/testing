c46844c23ed8033f15e47f33c32ebfce7ec8fc9d
Danial Hosseintabar <119340172+danialht@users.noreply.github.com>
2025-03-27 22:37:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from typing import Optional
-
-from fastapi import FastAPI
+r"""Dump Your Thoughts using Fast API"""
+from fastapi import FastAPI, Query
+from typing import Annotated
 
 app = FastAPI()
 
+all_messages = []
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/messages/')
+async def read_messages():
+    return all_messages
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post('/messages/')
+async def send_message(user: Annotated[str, Query(min_length = 5, max_length = 20, regex="^[a-z0-9]+$")] = "notgiven",
+                       message: Annotated[str, Query(min_length=1)] = "notgiven"):
+    all_messages.append({"user": user, "message": message})
+    return {"status": "success"}
