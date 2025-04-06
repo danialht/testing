ecfa7d4376d057c7f2a6ea0bf5a4a0c162fd54e2
Danial Hosseintabar <danial.ht10@gmail.com>
2025-04-06 02:56:54 -0400

    Bug fix

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 r"""Dump Your Thoughts using Fast API"""
 from fastapi import FastAPI, Query
 from typing import Annotated
+from pydantic import BaseModel
 
 app = FastAPI()
 
 all_messages = []
+
+class Message(BaseModel):
+    user: Annotated[str, Query(min_length = 5, max_length = 20, regex="^[a-z0-9]+$")]
+    message: Annotated[str, Query(min_length=1)]
 
 @app.get('/messages/')
 async def read_messages():
     return all_messages
 
 @app.post('/messages/')
-async def send_message(user: Annotated[str, Query(min_length = 5, max_length = 20, regex="^[a-z0-9]+$")] = "notgiven",
-                       message: Annotated[str, Query(min_length=1)] = "notgiven"):
-    all_messages.append({"user": user, "message": message})
+async def send_message(message: Message):
+    all_messages.append({"user": message.user, "message": message.message})
     return {"status": "success"}
